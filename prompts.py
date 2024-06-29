bd5c1b2d50a4a76b61eff6ca42de7c27322804e6
pascal-wolf <pwolf96@icloud.com>
2024-06-29 15:32:38 +0200

    feat: :sparkles: add prompt for quantitative approach

--- prompts.py
+++ prompts.py
@@ -33,5 +33,70 @@ User:
 COMPOUND_PROMPT = """
 You are given a question from a user. This question is a compound question, meaning it contains multiple questions.
 Please tell the user that currently we only support one question at a time and suggest the user how he/she can split the question into multiple single questions.
-Stay with the same questions the user asked and don't change the meaning of the questions.
+Stay with the same questions the user asked and don't change the meaning of the questions. Only write the suggested questions one time.
+"""
+
+QUANTITATIVE_PROMPT = """
+Given the following Redis database schema. Please return a query that can answer the question.
+Examples are:
+
+User question: How many users have the name Paul and are between 30 and 40 years old?
+Answer from you: @user:Paul @age:[30 40]
+
+User question: How many users are named Paul?
+Answer from you:  @user:Paul
+
+User question: How many reviews have more than 10 likes?
+Answer from you: @likes:[10 +inf]
+
+The correct syntax for a text and numeric question is the following:
+text_field @numeric_field:[value_numeric_field]
+
+Please always only return with the query! If you can't come up with a query only return the following two letters: na
+
+
+
+The database schema is the following:
+numeric:
+- name: score
+  no_index: false
+  sortable: false
+- name: likes
+  no_index: false
+  sortable: false
+- name: likes_weighted
+  no_index: false
+  sortable: false
+text:
+- name: created_date
+  no_index: false
+  no_stem: false
+  sortable: false
+  weight: 1
+  withsuffixtrie: false
+- name: weekday
+  no_index: false
+  no_stem: false
+  sortable: false
+  weight: 1
+  withsuffixtrie: false
+- name: contains_source_word
+  no_index: false
+  no_stem: false
+  sortable: false
+  weight: 1
+  withsuffixtrie: false
+- name: content
+  no_index: false
+  no_stem: false
+  sortable: false
+  weight: 1
+  withsuffixtrie: false
+"""
+
+REPHRASE_PROMPT = """
+Your only task is to formulate an answer based on the user question and answer you got:
+Make sure that you are specifically mentioning the number. If the answer is 'na' then please tell the user, that unfortunately,
+you couldn't come up with a response and ask him/her to reformualte the question.
+Only write the answer and do not say that you are now reformulating it!
 """
